38381c7cbe02d5f99d033ac440423a06a396f80a
Scott Schmitz <scottaschmitz@gmail.com>
2022-10-12 20:58:58 -0400

    Update to latest Android Studio

--- app/src/main/java/com/sschmitz/samplenav/ui/navigation/HomeGraph.kt
+++ app/src/main/java/com/sschmitz/samplenav/ui/navigation/HomeGraph.kt
@@ -1,22 +1,13 @@
 package com.sschmitz.samplenav.ui.navigation
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
-import com.sschmitz.samplenav.R
 import com.sschmitz.samplenav.ui.Route
 import com.sschmitz.samplenav.ui.home.HomeDetails
 import com.sschmitz.samplenav.ui.home.HomeOverview
