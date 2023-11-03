d757875233900aa02bde4679f9a1927eeced9b32
Scott Schmitz <sschmitz@michiganlabs.com>
2023-11-03 11:26:27 -0400

    add a 3rd screen to navigate into

--- app/src/main/java/com/sschmitz/samplenav/ui/navigation/HomeGraph.kt
+++ app/src/main/java/com/sschmitz/samplenav/ui/navigation/HomeGraph.kt
@@ -10,6 +10,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.navigation
 import com.sschmitz.samplenav.ui.Route
 import com.sschmitz.samplenav.ui.home.HomeDetails
+import com.sschmitz.samplenav.ui.home.HomeDetailsExtra
 import com.sschmitz.samplenav.ui.home.HomeOverview
 
 fun NavGraphBuilder.homeGraph(navController: NavHostController) {
@@ -20,13 +21,26 @@ fun NavGraphBuilder.homeGraph(navController: NavHostController) {
         composable(Route.HOME_OVERVIEW) {
             HomeOverview(
                 toDetails = { navController.navigate(Route.HOME_DETAILS) },
-                modifier = Modifier.fillMaxSize().padding(16.dp)
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
             )
         }
 
         composable(Route.HOME_DETAILS) {
             HomeDetails(
-                modifier = Modifier.fillMaxSize().padding(16.dp)
+                toExtraDetails = { navController.navigate(Route.HOME_DETAILS_EXTRA) },
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+            )
+        }
+
+        composable(Route.HOME_DETAILS_EXTRA) {
+            HomeDetailsExtra(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
             )
         }
     }
